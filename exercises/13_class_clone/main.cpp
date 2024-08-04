91a86bc9cc183e1988654cc79c068c2b2599141d
FailingSail <1660085056@qq.com>
2024-08-04 19:00:39 +0800

    update

--- exercises/13_class_clone/main.cpp
+++ exercises/13_class_clone/main.cpp
@@ -10,7 +10,7 @@ class DynFibonacci {
 
 public:
     // TODO: 实现动态设置容量的构造器
-    DynFibonacci(int capacity): cache(new size_t[capacity]), cached(0) {}
+    DynFibonacci(int capacity): cache(new size_t[capacity]), cached(2) {}
 
     // TODO: 实现复制构造器
     DynFibonacci(const DynFibonacci &a){
@@ -20,12 +20,13 @@ public:
 
     // TODO: 实现析构器，释放缓存空间
     ~DynFibonacci() {
-        if(cache)delete [] cache;
+        if(cache) delete [] cache;
+        cache = NULL;
     }
 
     // TODO: 实现正确的缓存优化斐波那契计算
     size_t get(int i) {
-        for (cache[0]=0,cache[1]=1,cached = 2; cached<=i; ++cached) {
+        for (cache[0]=0,cache[1]=1; cached<=i; ++cached) {
             cache[cached] = cache[cached - 1] + cache[cached - 2];
         }
         return cache[i];
